7589c0b9ac82b891c4fa9f2723d89ffd02349fce
Matthew Sells <matthew.m.sells@gmail.com>
2021-07-30 09:15:32 -0500

    set response default format

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_scope :user do
-    scope :v1 do
-      post '/users/sign_up', to: 'v1/registrations#create', as: :v1_user_registration
-      post '/users/sign_in', to: 'v1/sessions#create', as: :v1_user_session
+  # Set up user mapping but do not add any routes as these will be declared manually for
+  # multiple namespace usage
+  devise_for :user, only: %w[]
+
+  # Set default response type to ensure flash messages are not set
+  defaults format: :json do
+    devise_scope :user do
+      scope :v1 do
+        post '/users/sign_up', to: 'v1/registrations#create', as: :v1_user_registration
+        post '/users/sign_in', to: 'v1/sessions#create',      as: :v1_user_session
+      end
     end
   end
 end
--- spec/requests/v1/registrations_spec.rb
+++ spec/requests/v1/registrations_spec.rb
@@ -2,15 +2,31 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Rooms', type: :request do
-  before do
-    sign_in(user)
-  end
+RSpec.describe 'Registrations', type: :request do
+  describe 'POST /v1/users/sign_in' do
+    context 'when the params are valid' do
+      let(:params) do
+        {
+          user: {
+            email: 'user+1@example.com',
+            password: 'password',
+            password_confirmation: 'password'
+          }
+        }
+      end
 
-  describe 'POST /rooms' do
-    it 'displays the rooms index page' do
-      get rooms_path
-      expect(response).to render_template(:index)
+      it 'creates a new user' do
+        expect { post v1_user_registration_path, params: params }.to change(User, :count).by 1
+      end
+
+      it 'responds with the user json' do
+      end
+    end
+
+    context 'when the params are not valid' do
+    end
+
+    context 'when the user already exists' do
     end
   end
 end
