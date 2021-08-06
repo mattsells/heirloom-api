f8fa1e1f7b81e561416f3eea3f6b1555a6f30cdc
Matthew Sells <matthew.m.sells@gmail.com>
2021-08-06 15:17:55 -0500

    add recipe policy spec

--- spec/policies/recipe_policy_spec.rb
+++ spec/policies/recipe_policy_spec.rb
@@ -3,27 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe RecipePolicy, type: :policy do
-  subject { described_class }
-
-  let(:user) { User.new }
+  let(:user) { FactoryBot.create(:user) }
 
   permissions '.scope' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:scope) { Pundit.policy_scope!(user, Recipe) }
+
+    before do
+      FactoryBot.create_list(:recipe, 3, account: user.own_account)
+
+      second_account = FactoryBot.create(:account)
+      second_account.users << user
+
+      FactoryBot.create_list(:recipe, 3, account: second_account)
+      FactoryBot.create_list(:recipe, 3)
+    end
+
+    it 'returns only records that are linked to one of the users accounts' do
+      expect(scope.pluck(:account_id).uniq).to contain_exactly(*user.account_users.pluck(:account_id))
+    end
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :index? do
+    it 'permits' do
+      expect(described_class).to permit(user, Recipe)
+    end
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  permissions :show?, :create?, :update?, :destroy? do
+    context 'when the user is a member of the recipe account' do
+      let(:record) { FactoryBot.create(:recipe, account: user.own_account) }
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+      it 'permits' do
+        expect(described_class).to permit(user, record)
+      end
+    end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'when the user is not a member of the account' do
+      let(:record) { FactoryBot.create(:recipe) }
+
+      it 'does not permit' do
+        expect(described_class).to_not permit(user, record)
+      end
+    end
   end
 end
